8f578ea483c67e8595bb68a7f257e256240293a1
Darshan Kumar <itsdarshankumar@gmail.com>
2022-07-25 03:25:07 +0530

    script correct

--- script.sh
+++ script.sh
@@ -20,11 +20,11 @@
 
 	touch config/config.php
 	echo '<?php'>config/config.php
-	echo '$DB_HOST= '${DB_HOST}';'>>config/config.php
-	echo '$DB_PORT= '${DB_PORT}';'>>config/config.php
-	echo '$DB_NAME= '${DB_NAME}';'>>config/config.php
-	echo '$DB_USERNAME= '${DB_USERNAME}';'>>config/config.php
-	echo '$DB_PASSWORD= '${DB_PASSWORD}';'>>config/config.php
+    echo '$DB_HOST = "'$DB_HOST'";' >> config/config.php
+    echo '$DB_PORT = "'$DB_PORT'";' >> config/config.php
+    echo '$DB_NAME = "'$DB_NAME'";' >> config/config.php
+    echo '$DB_USERNAME = "'$DB_USERNAME'";' >> config/config.php
+    echo '$DB_PASSWORD = "'$DB_PASSWORD'";' >> config/config.php
 	echo '?>'>>config/config.php
 
 	cd public
